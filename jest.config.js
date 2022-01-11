5fae8e3312d172e870815d82692095b60fd0f206
KevinKra <kratok08@gmail.com>
2022-01-11 13:29:42 -0700

    install and setup tys-jest and react testing library

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,11 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: "ts-jest",
-  testEnvironment: "node",
+  testEnvironment: "jsdom",
+  globals: {
+    "ts-jest": {
+      tsconfig: "./tsconfig.jest.json",
+    },
+  },
+  setupFilesAfterEnv: ["<rootDir>/src/setuptests.ts"],
 };
